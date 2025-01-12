ab6395f732d2ff272d4a930a53bd4c4d17719338
Langelooo <masricoparati@gmail.com>
2025-01-12 19:31:50 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "C:\Users\mikae\veri\include\wx\wx.h"
+
+class MyApp : public wxApp {
+public:
+    virtual bool OnInit();
+};
+
+class MyFrame : public wxFrame {
+public:
+    MyFrame(const wxString& title);
+};
+
+wxIMPLEMENT_APP(MyApp);
+
+bool MyApp::OnInit() {
+    MyFrame* frame = new MyFrame("Hello wxWidgets");
+    frame->Show(true);
+    return true;
+}
+
+MyFrame::MyFrame(const wxString& title) : wxFrame(NULL, wxID_ANY, title) {
+}
